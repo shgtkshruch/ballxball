7d234c162e4f4eb9c34b5e8976a36f1c3e722866
shgtkshruch <shgtk.shruch@gmail.com>
2016-09-22 18:56:11 +0900

    Add contact page

---
+++ src/scripts/contact.js
@@ -0,0 +1,10 @@
+(() => {
+  'use strict';
+  $('.wpcf7-form-control').focus(function () {
+    $(this).parent().siblings('label').addClass('is-active');
+  }).focusout(function () {
+    if ($(this).val().length === 0) {
+      $(this).parent().siblings('label').removeClass('is-active');
+    }
+  });
+})();
